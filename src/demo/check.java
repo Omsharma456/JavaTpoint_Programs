48ebcaddd3effe41a157323c3767106c7a21ce55
Manish Sharma <omsharma571222@gmail.com>
2021-04-09 22:45:14 +0530

    pattern 1 code

---
+++ src/demo/check.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class check {
+    public static int mymeth(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    static class mysub {
+        public mysub() {
+            super();
+        }
+    }
+}
