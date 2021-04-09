0a862694402295c54b0cf7634a84e9bb21a407f6
Manish Sharma <omsharma571222@gmail.com>
2021-04-09 22:57:35 +0530

    Triangle rename

--- src/Pattern/Triangle.java
+++ src/Pattern/Triangle.java
@@ -2,7 +2,7 @@ package Pattern;
 
 import java.util.Scanner;
 
-public class patternTriangle1 {
+public class Triangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
